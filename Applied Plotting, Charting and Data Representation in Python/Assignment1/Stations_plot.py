7598f76889cdb2b2b116a389c14cd6b6bd87d64c
Shobhit Dalal <dalalshobhit@gmail.com>
2018-06-07 21:08:20 -0400

    Project files upload for plotting temperatures
    Plot record high & low temperatures for Markdale, Ontario region recorded in the period ranging from 2005-2015.

---
+++ Applied Plotting, Charting and Data Representation in Python/Assignment1/Stations_plot.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+import mplleaflet
+import pandas as pd
+
+def leaflet_plot_stations(binsize, hashid):
+
+    df = pd.read_csv('data/C2A2_data/BinSize_d{}.csv'.format(binsize))
+
+    station_locations_by_hash = df[df['hash'] == hashid]
+
+    lons = station_locations_by_hash['LONGITUDE'].tolist()
+    lats = station_locations_by_hash['LATITUDE'].tolist()
+
+    plt.figure(figsize=(8,8))
+
+    plt.scatter(lons, lats, c='r', alpha=0.7, s=200)
+
+    return mplleaflet.display()
+
+leaflet_plot_stations(400,'f92f364eaf87779ba90d1b8fe51d0982689343244dc8eb18bfc19c04')
