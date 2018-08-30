634a44e12136dc2ce89399e818ef38f9c1a5739c
Shobhit Dalal <dalalshobhit@gmail.com>
2018-08-29 20:40:48 -0400

    Upload updated code
    Commit updated code

--- Applied Plotting, Charting and Data Representation in Python/Project/Climate_Change_Effect.py
+++ Applied Plotting, Charting and Data Representation in Python/Project/Climate_Change_Effect.py
@@ -42,15 +42,50 @@ df.drop(df.columns[[4]], axis=1, inplace=True)
 # Second row will plot mean temperature for both region (January)
 # Third row will plot minimum temperature for both region (January)
 
+# Create a jan_temp dataframe with weather data only for month of January
 jan_temp = df[df['Month']==1]
-print(jan_temp.head(5))
 
+# Replace NaN values with averages of that columns
+jan_temp['Mean_Max_Toronto_(°C)'].fillna((jan_temp['Mean_Max_Toronto_(°C)'].mean()), inplace=True)
+jan_temp['Mean_Max_HallBeach_(°C)'].fillna((jan_temp['Mean_Max_HallBeach_(°C)'].mean()), inplace=True)
+#jan_temp['Mean_Mean_Toronto_(°C)'].fillna((jan_temp['Mean_Mean_Toronto_(°C)'].mean()), inplace=True)
+#jan_temp['Mean_Mean_HallBeach_(°C)'].fillna((jan_temp['Mean_Mean_HallBeach_(°C)'].mean()), inplace=True)
+#jan_temp['Mean_Min_Toronto_(°C)'].fillna((jan_temp['Mean_Min_Toronto_(°C)'].mean()), inplace=True)
+#jan_temp['Mean_Min_HallBeach_(°C)'].fillna((jan_temp['Mean_Min_HallBeach_(°C)'].mean()), inplace=True)
+print(jan_temp.head(15))
+
+# Store temperature data in different dataframes
+max_toronto = jan_temp['Mean_Max_Toronto_(°C)']
+max_hallbeach = jan_temp['Mean_Max_HallBeach_(°C)']
+#mean_toronto = jan_temp['Mean_Mean_Toronto_(°C)']
+#mean_hallbeach = jan_temp['Mean_Mean_HallBeach_(°C)']
+#min_toronto = jan_temp['Mean_Min_Toronto_(°C)']
+#min_hallbeach = jan_temp['Mean_Min_HallBeach_(°C)']
+
+# Create a figure canvas to plot subplots
 fig = plt.figure()
+ax1 = fig.add_subplot(311)
+ax2 = fig.add_subplot(312)
+ax3 = fig.add_subplot(313)
 
-plt.subplot(3, 1, 1)
-plt.plot(jan_temp['Year'], jan_temp['Mean_Max_Toronto_(°C)'], '-')
-plt.title('Comparison of mean maximum temperatures of Toronto, Ontario and Hall Beach, Nunavut')
-plt.ylabel('Temperatue (°C)')
+# Plot 1st subplot to compare average monthly maximum temperature
+p11 = ax1.plot(jan_temp['Year'], max_toronto, '-', color='coral')
+p12 = ax1.plot(jan_temp['Year'], max_hallbeach, '-', color='crimson')
+ax1.set_title('Maximum temp. of Toronto vs. Hall Beach (January)')
+ax1.set_ylabel('Temp. (°C)')
+
+# Plot 2nd subplot to compare average monthly mean temperature
+#p21 = ax2.plot(jan_temp['Year'], jan_temp['Mean_Mean_Toronto_(°C)'], '-', color='gold')
+#p22 = ax2.plot(jan_temp['Year'], jan_temp['Mean_Mean_HallBeach_(°C)'], '-', color='goldenrod')
+#ax2.set_title('Mean temp. of Toronto vs. Hall Beach (January)')
+#ax2.set_ylabel('Temp. (°C)')
 
 
+# Plot 1st subplot to compare average monthly maximum temperature
+#p31 = ax3.plot(jan_temp['Year'], jan_temp['Mean_Min_Toronto_(°C)'], '-', color='darkblue')
+#p32 = ax3.plot(jan_temp['Year'], jan_temp['Mean_Min_HallBeach_(°C)'], '-', color='lightblue')
+#plt.title('Minimum temperatures of Toronto vs. Hall Beach (January)')
+#plt.ylabel('Temperatue (°C)')
+
+# Save the plot as a png file
 fig.savefig('outputFigure.png')
